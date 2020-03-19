83ce5733b91c13a3de8a6d20a61ecb4b01c3bcc7
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-03-19 11:55:14 +0300

    increased wait for keyboard shown timeout and reordered steps in textbox test

--- src/test/java/samples/android/web/WebTextBoxTest.java
+++ src/test/java/samples/android/web/WebTextBoxTest.java
@@ -10,35 +10,42 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WebTextBoxTest extends AndroidWebTest {
+    private static final String VALUE_TO_SUBMIT = "quality assurance";
+
     @Test(groups = "web")
     public void testTextBoxInteraction() {
         AqualityServices.getApplication().getDriver().get("https://wikipedia.org");
         ITextBox txbSearch = AqualityServices.getElementFactory().getTextBox(By.id("searchInput"), "Search");
         txbSearch.state().waitForClickable();
-        txbSearch.click();
-        Assert.assertTrue(isKeyboardShown(true), "Keyboard should be shown when click successful");
-        txbSearch.unfocus();
-        Assert.assertFalse(isKeyboardShown(false), "Keyboard should not be shown when unfocus successful");
-        txbSearch.focus();
-        Assert.assertTrue(isKeyboardShown(true), "Keyboard should be shown when focus successful");
-        final String valueToSubmit = "quality assurance";
-        txbSearch.type(valueToSubmit);
-        Assert.assertEquals(valueToSubmit, txbSearch.getValue(), "Submitted value should match to expected");
+        txbSearch.type(VALUE_TO_SUBMIT);
+        Assert.assertEquals(VALUE_TO_SUBMIT, txbSearch.getValue(), "Submitted value should match to expected");
         txbSearch.clear();
         Assert.assertEquals("", txbSearch.getValue(), "Value should be cleared");
-        txbSearch.typeSecret(valueToSubmit);
-        Assert.assertEquals(valueToSubmit, txbSearch.getValue(), "Submitted value should match to expected");
-        txbSearch.clearAndType(valueToSubmit);
-        Assert.assertEquals(valueToSubmit, txbSearch.getValue(), "Submitted value should match to expected");
-        txbSearch.clearAndTypeSecret(valueToSubmit);
-        Assert.assertEquals(valueToSubmit, txbSearch.getValue(), "Submitted value should match to expected");
+        txbSearch.click();
+        checkUnfocus(txbSearch);
+        txbSearch.focus();
+        Assert.assertTrue(isKeyboardShown(true), "Keyboard should be shown when focus successful");
+        txbSearch.typeSecret(VALUE_TO_SUBMIT);
+        Assert.assertEquals(VALUE_TO_SUBMIT, txbSearch.getValue(), "Submitted value should match to expected");
+        txbSearch.clearAndType(VALUE_TO_SUBMIT);
+        Assert.assertEquals(VALUE_TO_SUBMIT, txbSearch.getValue(), "Submitted value should match to expected");
+        txbSearch.clearAndTypeSecret(VALUE_TO_SUBMIT);
+        Assert.assertEquals(VALUE_TO_SUBMIT, txbSearch.getValue(), "Submitted value should match to expected");
         txbSearch.sendKeys(Keys.ENTER);
         Assert.assertTrue(txbSearch.state().waitForNotDisplayed(), "text field should disappear after the submit");
     }
 
+    private void checkUnfocus(ITextBox txbSearch) {
+        txbSearch.unfocus();
+        Assert.assertFalse(isKeyboardShown(false), "Keyboard should not be shown when unfocus successful");
+    }
+
     private boolean isKeyboardShown(boolean expectedStateToWait) {
         boolean waitResult = AqualityServices.getConditionalWait()
-                .waitFor(driver -> ((AndroidDriver<AndroidElement>)driver).isKeyboardShown() == expectedStateToWait);
+                .waitFor(driver -> ((AndroidDriver<AndroidElement>)driver).isKeyboardShown() == expectedStateToWait,
+                        AqualityServices.getConfiguration().getTimeoutConfiguration().getCommand(),
+                        AqualityServices.getConfiguration().getTimeoutConfiguration().getPollingInterval().multipliedBy(10),
+                        String.format("is keyboard shown condition should be %s", expectedStateToWait));
         return expectedStateToWait == waitResult;
     }
 }
