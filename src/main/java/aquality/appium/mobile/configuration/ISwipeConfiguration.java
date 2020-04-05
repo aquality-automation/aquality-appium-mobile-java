dd33cd4b2eab2d447ef5e32c9abc2301b0d8cd77
nviarbitski <nviarbitski@dsycloud.com>
2020-04-06 01:30:19 +0300

    Fixing Swipe Configs;
    - Updated ISwipeCongifuration with new settings.
    - Updated SwipeConfiguration with implementation for new settings.
    - Updating "swipe" object in test.resources.settings.json.
    - Adding "swipe" object in test.resources.settings.androidwebsession.json.
    - Adding "swipe" object in main.resources.settings.json.

--- src/main/java/aquality/appium/mobile/configuration/ISwipeConfiguration.java
+++ src/main/java/aquality/appium/mobile/configuration/ISwipeConfiguration.java
@@ -2,8 +2,78 @@ package aquality.appium.mobile.configuration;
 
 import java.time.Duration;
 
+/**
+ * Describes Swipe settings.
+ */
 public interface ISwipeConfiguration {
+    /**
+     * Gets number of retries to perform swipe.
+     *
+     * @return arguments map.
+     */
     int getRetries();
 
+    /**
+     * Gets the timeout to perform a swipe.
+     *
+     * @return timeout to perform swipe.
+     */
     Duration getTimeout();
+
+    /**
+     * Gets the coefficient to calculate the 'x' coordinate of top point for swipe action.
+     *
+     * @return coefficient to calculate the 'x' coordinate of top point for swipe action
+     */
+    double getHorizontalSwipeTopPointXCoefficient();
+
+    /**
+     * Gets the coefficient to calculate the 'x' coordinate of bottom point for swipe action.
+     *
+     * @return coefficient to calculate the 'x' coordinate of bottom point for swipe action
+     */
+    double getHorizontalSwipeBottomPointXCoefficient();
+
+    /**
+     * Gets the coefficient to calculate the 'y' coordinate of top point for swipe action.
+     *
+     * @return coefficient to calculate the 'y' coordinate of top point for swipe action
+     */
+    double getHorizontalSwipeTopPointYCoefficient();
+
+    /**
+     * Gets the coefficient to calculate the 'y' coordinate of bottom point for swipe action.
+     *
+     * @return coefficient to calculate the 'y' coordinate of bottom point for swipe action
+     */
+    double getHorizontalSwipeBottomPointYCoefficient();
+
+    /**
+     * Gets the coefficient to calculate the 'x' coordinate of top point for swipe action.
+     *
+     * @return timeout to perform swipe.
+     */
+    double getVerticalSwipeLeftPointXCoefficient();
+
+    /**
+     * Gets the coefficient to calculate the 'x' coordinate of right-hand point for swipe action.
+     *
+     * @return coefficient to calculate the 'x' coordinate of right-hand point for swipe action
+     */
+    double getVerticalSwipeRightPointXCoefficient();
+
+    /**
+     * Gets the coefficient to calculate the 'y' coordinate of left-hand point for swipe action.
+     *
+     * @return coefficient to calculate the 'y' coordinate of left-hand point for swipe action
+     */
+    double getVerticalSwipeLeftPointYCoefficient();
+
+
+    /**
+     * Gets the coefficient to calculate the 'y' coordinate of right-hand point for swipe action.
+     *
+     * @return coefficient to calculate the 'y' coordinate of right-hand point for swipe action
+     */
+    double getVerticalSwipeRightPointYCoefficient();
 }
--- src/main/java/aquality/appium/mobile/configuration/SwipeConfiguration.java
+++ src/main/java/aquality/appium/mobile/configuration/SwipeConfiguration.java
@@ -8,11 +8,35 @@ import java.time.Duration;
 public class SwipeConfiguration implements ISwipeConfiguration {
     private final int retries;
     private final Duration timeout;
+    private final double horizontalSwipeTopPointXCoefficient;
+    private final double horizontalSwipeBottomPointXCoefficient;
+    private final double horizontalSwipeTopPointYCoefficient;
+    private final double horizontalSwipeBottomPointYCoefficient;
+    private final double verticalSwipeLeftPointXCoefficient;
+    private final double verticalSwipeRightPointXCoefficient;
+    private final double verticalSwipeLeftPointYCoefficient;
+    private final double verticalSwipeRightPointYCoefficient;
 
     @Inject
     public SwipeConfiguration(ISettingsFile settingsFile) {
         this.retries = Integer.parseInt(settingsFile.getValue("/swipe/retries").toString());
         this.timeout = Duration.ofSeconds(Long.parseLong(settingsFile.getValue("/swipe/timeout").toString()));
+        this.horizontalSwipeTopPointXCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/horizontalSwipeTopPointXCoefficient").toString());
+        this.horizontalSwipeBottomPointXCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/horizontalSwipeBottomPointXCoefficient").toString());
+        this.horizontalSwipeTopPointYCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/horizontalSwipeTopPointYCoefficient").toString());
+        this.horizontalSwipeBottomPointYCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/horizontalSwipeBottomPointYCoefficient").toString());
+        this.verticalSwipeLeftPointXCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/verticalSwipeLeftPointXCoefficient").toString());
+        this.verticalSwipeRightPointXCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/verticalSwipeRightPointXCoefficient").toString());
+        this.verticalSwipeLeftPointYCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/verticalSwipeLeftPointYCoefficient").toString());
+        this.verticalSwipeRightPointYCoefficient = Double.parseDouble(
+                settingsFile.getValue("/swipe/verticalSwipeRightPointYCoefficient").toString());
     }
 
     @Override
@@ -23,5 +47,45 @@ public class SwipeConfiguration implements ISwipeConfiguration {
     @Override
     public Duration getTimeout() {
         return this.timeout;
+    }
+
+    @Override
+    public double getHorizontalSwipeTopPointXCoefficient() {
+        return this.horizontalSwipeTopPointXCoefficient;
+    }
+
+    @Override
+    public double getHorizontalSwipeBottomPointXCoefficient() {
+        return this.horizontalSwipeBottomPointXCoefficient;
+    }
+
+    @Override
+    public double getHorizontalSwipeTopPointYCoefficient() {
+        return this.horizontalSwipeTopPointYCoefficient;
+    }
+
+    @Override
+    public double getHorizontalSwipeBottomPointYCoefficient() {
+        return this.horizontalSwipeBottomPointYCoefficient;
+    }
+
+    @Override
+    public double getVerticalSwipeLeftPointXCoefficient() {
+        return this.verticalSwipeLeftPointXCoefficient;
+    }
+
+    @Override
+    public double getVerticalSwipeRightPointXCoefficient() {
+        return this.verticalSwipeRightPointXCoefficient;
+    }
+
+    @Override
+    public double getVerticalSwipeLeftPointYCoefficient() {
+        return this.verticalSwipeLeftPointYCoefficient;
+    }
+
+    @Override
+    public double getVerticalSwipeRightPointYCoefficient() {
+        return this.verticalSwipeRightPointYCoefficient;
     }
 }
