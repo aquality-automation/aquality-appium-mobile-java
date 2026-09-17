abbd0eed50808be3c20a164b9575805f099f3d18
Aleksey Meleshko <a2.meleshko@a1qa.com>
2026-09-17 15:41:44 +0200

    Stabilize tests

--- src/test/java/samples/android/web/WebTextBoxTest.java
+++ src/test/java/samples/android/web/WebTextBoxTest.java
@@ -2,6 +2,7 @@ package samples.android.web;
 
 import aquality.appium.mobile.application.AqualityServices;
 import aquality.appium.mobile.elements.interfaces.IButton;
+import aquality.appium.mobile.elements.interfaces.ILabel;
 import aquality.appium.mobile.elements.interfaces.ITextBox;
 import aquality.selenium.core.configurations.ITimeoutConfiguration;
 import aquality.selenium.core.elements.ElementState;
@@ -9,6 +10,7 @@ import aquality.selenium.core.elements.ElementsCount;
 import aquality.selenium.core.elements.interfaces.IElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -20,15 +22,26 @@ public class WebTextBoxTest extends AndroidWebTest {
     private static final String VALUE_TO_SUBMIT = "quality assurance";
     private static final ITextBox txbSearch = AqualityServices.getElementFactory().getTextBox(By.id("searchInput"), "Search");
     private static final IButton btnOverlayToggle = AqualityServices.getElementFactory().getButton(By.className("button-collapse"), "Toggle Overlay");
+    private static final ILabel lblOverlayMessage = AqualityServices.getElementFactory().getLabel(By.className("overlay-banner-main-message-greeting"), "Overlay Message");
 
     private void closeBanner() {
-        if (btnOverlayToggle.state().isDisplayed() && "true".equals(btnOverlayToggle.getAttribute("aria-expanded"))) {
+        if (btnOverlayToggle.state().isDisplayed() && lblOverlayMessage.state().isDisplayed()) {
             btnOverlayToggle.click();
         }
         Optional<IButton> btnCloseBanner = AqualityServices.getElementFactory()
                 .findElements(By.cssSelector("button[class*=close]"), "Close banner", IButton.class, ElementsCount.ANY, ElementState.DISPLAYED)
                 .stream().findFirst();
         btnCloseBanner.ifPresent(IElement::click);
+    }
+
+    private void clickSearchTextBox() {
+        try {
+            txbSearch.click();
+        } catch (ElementClickInterceptedException e) {
+            AqualityServices.getLogger().fatal(e.getMessage(), e);
+            closeBanner();
+            txbSearch.click();
+        }
     }
 
     @Test
@@ -41,7 +54,7 @@ public class WebTextBoxTest extends AndroidWebTest {
         txbSearch.clear();
         Assert.assertEquals(txbSearch.getValue(), "", "Value should be cleared");
         closeBanner();
-        txbSearch.click();
+        clickSearchTextBox();
         checkUnfocus();
         txbSearch.focus();
         Assert.assertTrue(isKeyboardShown(true), "Keyboard should be shown when focus successful");
